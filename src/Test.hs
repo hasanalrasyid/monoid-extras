ab10518178f3858bd1562723445b44c5764069dc
Hasan al Rasyid <hasanalrasyid@home>
2022-03-19 17:08:25 +0900

    update

--- src/Test.hs
+++ src/Test.hs
@@ -1,18 +1,12 @@
 {-# LANGUAGE StandaloneDeriving #-}
 
+module Test where
+
 import Test.QuickCheck
 import Control.Applicative
 
 import Data.Monoid.Cut
 import Data.Semigroup
-
-instance Arbitrary a => Arbitrary (Sum a) where
-  arbitrary = Sum <$> arbitrary
-
-instance Arbitrary a => Arbitrary (Cut a) where
-  arbitrary = oneof [ Uncut <$> arbitrary
-                    , liftA2 (:||:) arbitrary arbitrary
-                    ]
 
 deriving instance Eq a => Eq (Cut a)
 
